5e33bcc249732d12afc3e98470d8ade1fbc75d36
Devon Burnham <novedevo@gmail.com>
2020-04-23 13:39:08 -0700

    update

--- covid-analysis.py
+++ covid-analysis.py
@@ -3,26 +3,30 @@ from datetime import datetime, date
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+from scipy.optimize import curve_fit
 
-USER = True
+USER = False
 
 Province = None
+Overarch = None
+Country = None
 
 if USER:
+    Overarch = input("Please enter a continent, 'world', or leave blank for a country/province: ")
     Country = input("Please enter your country of choice in the formal style: ")
-
     Province = input("Specify a province or leave blank: ")
 else:
+    #Overarch = "World"
     Country = "Canada"
     Province = "British Columbia"
     
 #if not Province:
 #    Province = None
 
-path_to_data = "C:\\Users\\noved\\Documents\\Actual Documents\\Programming\\COVID-19\\csse_covid_19_data\\csse_covid_19_time_series\\time_series_19-covid-"
-confirmedcsvpath = path_to_data + "Confirmed.csv"
-deathscsvpath = path_to_data + "Deaths.csv"
-recoveredcsvpath = path_to_data + "Recovered.csv"
+path_to_data = "C:\\Users\\noved\\Documents\\Actual Documents\\Programming\\COVID-19\\csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_"
+confirmedcsvpath = path_to_data + "confirmed_global.csv"
+deathscsvpath = path_to_data + "deaths_global.csv"
+recoveredcsvpath = path_to_data + "recovered_global.csv"
 
 topLine = "Province/State, Country/Region, Lat, Long, 1/22/20, 1/23/20, etc"
 
@@ -48,7 +52,9 @@ with open(confirmedcsvpath, 'r') as confirmedcsv: # autocloses after it's finish
     for row in myReader:
         #row={k: v for k, v in row.items() if v is not None}
         #pass
-        if not row["Country/Region"].lower() == Country.lower():
+        if Overarch and Overarch.lower() == 'world':
+            pass
+        elif not row["Country/Region"].lower() == Country.lower():
             continue
         if Province:
             Region = Province + ", " + Country
@@ -72,10 +78,13 @@ with open(confirmedcsvpath, 'r') as confirmedcsv: # autocloses after it's finish
                     #if not (i%7):
                     #    print(" ")
         else:
-            Region = Country
+            if Country:
+                Region = Country
+            else:
+                Region = Overarch
             #print(row["Country/Region"]+ ": ")
             newrow={k: v for k, v in row.items() if isinstance(k, date)}
-            print(row)
+            #print(row)
             for iii, (k,v) in enumerate(newrow.items()):
                 if firstRow:
                     cases.append(int(v))
@@ -89,8 +98,20 @@ with open(confirmedcsvpath, 'r') as confirmedcsv: # autocloses after it's finish
 #ax.bar(dates, cases, width=0.5)
 #ax.xaxis_date()
 
+def func(x, a, b, c):
+    return a * np.exp(b * x) + c
+
+xlen = range(len(dates))
+x=np.array(xlen)
+y=np.array(cases)
+yn=func(x,0,0,0)
+#np.polyfit(np.log(x+1), y, 1)
+
+popt, pcov = curve_fit(func, x, y)
+
 plt.scatter(dates, cases)
 plt.plot(dates, cases)
+#plt.plot(dates, func(x, *popt), 'r-', label="Fitted Curve")
 plt.grid()
 plt.xlabel("Date")
 plt.ylabel("Confirmed Cases")
